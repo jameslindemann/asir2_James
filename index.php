cd6e5b28384760c64d845b2b3c5dfa471371327c
unknown <jameslindemann>
2019-09-23 17:28:35 +0200

    Formulario

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 DER JAMES </br>
-<a href="http://192.168.0.34/index.php">KEVINASO PEPINASO</a> </br>
-<a href="http://192.168.0.37/index.php">KIM PUTA AMA</a> </br>
+<a href="http://192.168.0.48/Asir2_Kevin/">KEVINASO PEPINASO</a> </br>
+<a href="http://192.168.0.37/Asir2_Kimberly/">KIM PUTA AMA</a> </br>
 
 <?php
 	if(isset($_GET['email']))
 		print_r($_GET);
+	
+	#SaveSubs
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']."\r\n");
+	fclose($f)
 ?>
 
 <form>
