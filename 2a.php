c6e8e155198644026942c816b2b0bbe51cb3eae5
unknown <jameslindemann>
2019-10-07 16:02:32 +0200

    Variables

---
+++ 2a.php
@@ -0,0 +1,23 @@
+<?php
+$a=13;
+$b=10;
+echo '<div>';
+echo 'SUMA: '.$a.' + '.$b.' = ';
+echo $a+$b;
+echo '</div>';
+
+echo '<div>';
+echo 'RESTA: '.$b.' - '.$a.' = ';
+echo $b-$a;
+echo '</div>';
+
+echo '<div>';
+echo 'PRODUCTO: '.$a.' * '.$b.' = ';
+echo $a*$b;
+echo '</div>';
+
+echo '<div>';
+echo 'COCIENTE: '.$b.' / '.$a.' = ';
+echo $b/$a;
+echo '</div>';
+?>
