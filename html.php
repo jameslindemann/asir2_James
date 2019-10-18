b55305f5c04213c615dc2fe0656314b888a74105
unknown <jameslindemann>
2019-10-18 21:22:14 +0200

    Pagina HTML

---
+++ html.php
@@ -0,0 +1,61 @@
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>PAGINA HTML</title>
+</head>
+
+<body>
+
+<center>
+
+<h1>PROVINCIAS BAÑADAS POR EL CANTABRICO</h1>
+
+<img src="mapa.jpg" alt="Mapa Politico de España" style="width:400px;height:300px"/>
+
+</center>
+
+<h2 style="color:brown; font-size:22px";><b><u>
+Estas son todas las provincias que son bañadas por el Mar Cantabrico:
+</b></u></h2>
+
+<form>
+	<input>
+	<input type="submit">
+</form>
+
+<table style="font-size:20px;"border="1">
+<tr>
+<th> Comunidades </th>
+<th> Provincias </th>
+</tr>
+
+<tr style="color:blue">
+<td> Galicia </td>
+<td> A Coruña </td>
+</tr>
+
+<tr style="color:blue">
+<td> Galicia </td>
+<td> Lugo </td>
+</tr>
+
+<tr style="color:F4BD02">
+<td> Asturias </td>
+<td> Asturias </td>
+</tr>
+
+<tr style="color:red">
+<td> Cantabria </td>
+<td> Cantabria </td>
+</tr>
+
+</table>
+
+<p style="color:E502F4; font-size:20px";><b>
+Como podemos ver las provincias que son bañadas por el Cantabrico son Lugo, A Coruña y Asturias y Cantabria (estas dos ultimas son uniprovinciales)
+</b></p>
+
+</body>
+
+</html>
